c1c8bd23fd95a304f90949fb42607448eecb5632
Kanishka <kanishkadilshan861@gmail.com>
2024-03-04 21:21:55 +0530

    routing works fine, can be used for appointment landing page when modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './App.css';
-import UserForm from './UserForm';
+import {useNavigate} from 'react-router-dom';  {/* this is react navigator hook */}
 
 
 function App() {
+    const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
-        <UserForm/>
+<h1>Welcome</h1>
+<button className='user-button' onClick={() => navigate('/users')}>Users</button>   {/* giving arguement to the path defined in the index.js */}
       </header>
     </div>
   );
--- src/UserForm.js
+++ src/UserForm.js
@@ -1,24 +1,86 @@
-import {Grid, Typography} from "@mui/material";
+import { Button, Grid, Typography, Input } from "@mui/material";
 
 const UserForm = props => {
-return(
-        <Grid
-        container
-        spacing={2}
-        sx={{
-            backgroundColor: '#ffffff',
-            marginBottom: '30px',
-            display: "block",
-        }}
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: '#ffffff',
+        marginBottom: '30px',
+        display: "block",
+      }}
+    >
+
+      <Grid item xs={12}>
+        <Typography variant="h1" sx={{ color: '#000000' }}>User Form</Typography>
+      </Grid>
+
+      <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+        <Typography
+          component={'label'}
+          htmlFor='id'
+          sx={{
+            color: '#000000',
+            marginRight: 'right',
+            fontSize: '16.0',
+            width: '100px',
+            display: 'block',
+          }}
         >
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id='id'
+          name="id"
+          sx={{ width: '400px' }}
+          value={""}
+          onChange={e => { }}
+        />
+      </Grid>
 
-            <Grid
-             item xs={12}>
-            <Typography component={h1} sx={{color: '#000000'}}>User Form</Typography>
-                </Grid>
-        </Grid>
-);
+      <Grid item xs={12} sm={6} sx={{ display: 'flex' }}>
+        <Typography
+          component={'label'}
+          htmlFor='name'
+          sx={{
+            color: '#000000',
+            marginRight: 'right',
+            fontSize: '16.0',
+            width: '100px',
+            display: 'block',
+          }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id='name'
+          sx={{ width: '400px' }}
+          value={""}
+          onChange={e => { }}
+        />
+      </Grid>
 
+      <Button
+        sx={{
+          margin: 'auto',
+          marginBottom: '20px',
+          backgroundColor: '#00c6ec',
+          color: '#000000',
+          marginLeft: '15px',
+          marginTop: '20px',
+          '&:hover': {
+            opacity: '0.7',
+            backgroundColor: '#00c6ec',
+          }
+        }}
+      >
+        Add
+      </Button>
+    </Grid>
+  );
 }
 
 export default UserForm;
